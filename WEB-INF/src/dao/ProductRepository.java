76393bdacdd7b4f066779c56bbfa91212094c944
이준광 <jimmy010011@gmail.com>
2023-11-15 02:11:25 +0000

    .

--- WEB-INF/src/dao/ProductRepository.java
+++ WEB-INF/src/dao/ProductRepository.java
@@ -12,38 +12,37 @@ public class ProductRepository {
    }
 
 	public ProductRepository() {
-		Product phone = new Product("P1234", "아디다스 토바코 그루엔 메사 브라운 데저트", 139000);
-		phone.setDescription("출시일23/07/28");
-		phone.setCategory("shose");
-		phone.setManufacturer("Adidas");
-		phone.setUnitsInStock(1000);
-		phone.setCondition("New");
-        phone.setFilename("P1234.jpg");
+		Product shoe1 = new Product("P1234", "아디다스 토바코 그루엔 메사 브라운 데저트", 139000);
+        shoe1.setDescription("출시일 23/07/28");
+        shoe1.setCategory("shoes"); // 오탈자 수정: "shose" → "shoes"
+        shoe1.setManufacturer("Adidas");
+        shoe1.setUnitsInStock(1000);
+        shoe1.setCondition("New");
+        shoe1.setFilename("P1234.jpg");
 
-        
-        Product notebook = new Product("P1235", "나이키 에어포스 1 '07 WB 플랙스", 169000);
-		notebook.setDescription("19/09/10");
-		notebook.setCategory("shose");
-		notebook.setManufacturer("adidas");
-		notebook.setUnitsInStock(1000);
-		notebook.setCondition("Refurbished");
-        phone.setFilename("P1235.jpg");
+        Product shoe2 = new Product("P1235", "나이키 에어포스 1 '07 WB 플랙스", 169000);
+        shoe2.setDescription("19/09/10");
+        shoe2.setCategory("shoes");
+        shoe2.setManufacturer("Nike"); // 제조사 이름 수정: "adidas" → "Nike"
+        shoe2.setUnitsInStock(1000);
+        shoe2.setCondition("Refurbished");
+        shoe2.setFilename("P1235.jpg");
 
+        Product shoe3 = new Product("P1236", "아디다스 삼바 OG 블랙 화이트 검", 135000);
+        shoe3.setDescription("18/06/01");
+        shoe3.setCategory("shoes");
+        shoe3.setManufacturer("Adidas");
+        shoe3.setUnitsInStock(1000);
+        shoe3.setCondition("Old");
+        shoe3.setFilename("P1236.jpg");
 
-		Product tablet = new Product("P1236", "아디다스 삼바 OG 블랙 화이트 검", 135000);
-		tablet.setDescription("18/06/01");
-		tablet.setCategory("shose");
-		tablet.setManufacturer("adidas");
-		tablet.setUnitsInStock(1000);
-		tablet.setCondition("Old");
-        phone.setFilename("P1236.jpg");
 
 		
 		// 위와 같이 상품 초기화 하고 아래에 상품을 추가
 
-		listOfProducts.add(phone);
-		listOfProducts.add(notebook);
-		listOfProducts.add(tablet);
+		listOfProducts.add(shoe3);
+		listOfProducts.add(shoe2);
+		listOfProducts.add(shoe1);
 		// listOfProducts.add(상품명);
 	}
 	public ArrayList<Product> getAllProducts() {
